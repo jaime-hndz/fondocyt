519a1e766d920f87452b1825ac7a39bdda25b954
Jaime <jaime251102@gmail.com>
2025-09-09 00:57:34 -0400

    fix header and form  url

--- src/screens/public/tools/ParticipacionCiudadanaScreen.tsx
+++ src/screens/public/tools/ParticipacionCiudadanaScreen.tsx
@@ -2,7 +2,7 @@
 export const ParticipacionCiudadanaScreen = () => {
   return (
         <div className="">
-          <iframe className="w-full h-200 static x-<-10px>" src="https://lightgray-ibis-213262.hostingersite.com" />
+          <iframe className="w-full h-200 static x-<-10px>" src="https://foro.observatoriohaina.do" />
         </div>
     
   )
